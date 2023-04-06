55efe4a8dfd2ec4767c37eccbad931ff8d3f5d33
heartdoc516 <heartdoc855@gmail.com>
2023-04-06 11:02:48 -0400

    removed boat and items

--- script.js
+++ script.js
@@ -6,25 +6,18 @@ const layer5 = document.getElementById("layer-5");
 const layer6 = document.getElementById("layer-6");
 const layer7 = document.getElementById("layer-7");
 const layer8 = document.getElementById("layer-8");
-const boat1 = document.getElementById("boat-1");
-const boat2 = document.getElementById("boat-2");
-const boat3 = document.getElementById("boat-3");
-
 
 function parallax() {
-const scrollValue = window.scrollY 
-layer1.style.left = "-" + scrollValue * 0.1 + "px";
-layer2.style.top = scrollValue * 0.5 + "px";
-layer2.style.left = scrollValue * 0.05 + "px";
-layer3.style.top = scrollValue * 0.7 + "px";
-layer4.style.top = scrollValue * 0.96 + "px";
-layer5.style.top = scrollValue * 0.98 + "px";
-layer6.style.top = scrollValue * 0.99 + "px";
-layer7.style.top = scrollValue * 0.99 + "px";
-layer8.style.top = scrollValue + "px";
-boat1.style.top = scrollValue * 0.92 + "px"
-boat2.style.top = (scrollValue * 0.94)-80 + "px"
-boat3.style.top = (scrollValue * 0.96)-50 + "px"
+  const scrollValue = window.scrollY;
+  layer1.style.left = "-" + scrollValue * 0.1 + "px";
+  layer2.style.top = scrollValue * 0.5 + "px";
+  layer2.style.left = scrollValue * 0.05 + "px";
+  layer3.style.top = scrollValue * 0.7 + "px";
+  layer4.style.top = scrollValue * 0.96 + "px";
+  layer5.style.top = scrollValue * 0.98 + "px";
+  layer6.style.top = scrollValue * 0.99 + "px";
+  layer7.style.top = scrollValue * 0.99 + "px";
+  layer8.style.top = scrollValue + "px";
 }
 
 function reveal() {
@@ -44,8 +37,5 @@ function reveal() {
   }
 }
 
-window.addEventListener("scroll", parallax,);
-window.addEventListener("scroll", reveal,);
-
-
-
+window.addEventListener("scroll", parallax);
+window.addEventListener("scroll", reveal);
